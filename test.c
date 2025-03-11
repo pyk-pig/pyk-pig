2c453b978186d083c919e93fda658681285e8801
pyk-pig <2987994541@qq.com>
2025-03-11 20:15:55 +0800

    C of zhrng

--- test.c
+++ test.c
@@ -1,5 +1,27 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-	printf("hello,world!");
+	// int a = 0x66;
+	// int *p;
+	// p = &a;
+	// printf("address size of p = %d\n", sizeof(p));
+	// printf("address size of a = %d\n", sizeof(a));
+	// printf("%x\n", a);//十六进制形式
+	// printf("%x\n", p);
+	// printf("%x\n", *p);
+	// (*p)++;
+	// printf("*p++ = %x\n", *p);
+	// p++;
+	// printf("%x\n", p);
+	
+	//char a[] = {0x33,0x44,0x55,0x66};
+	malloc(3*4);
+	
+	char *p;
+	p = a;
+	printf("a[0] = %x\n", a[0]);
+	printf("a[1] = %x\n", *(a+1));
+	printf("a[2] = %x\n", *(p+2));
+	return 0;
 }
